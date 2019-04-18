d1b99d11e73497accfa7a716690ca09a3f5b65b6
wei <gaowei19951004@gmail.com>
2019-04-17 23:37:09 -0400

    before more testing on spartan dataset

---
+++ dataproc/spartan_singleobj_database.py
@@ -0,0 +1,222 @@
+import attr
+import os
+from typing import List
+import yaml
+import cv2
+from utils.imgproc import PixelCoord, mask2bbox
+from dataproc.abstract_db import AbstractMaskDatabase, ImageWithAnnotation, AnnotationEntry
+
+
+@attr.s
+class SpartanSingleObjMaskDatabaseConfig:
+    # ${pdc_data_root}/logs_proto/2018-10....
+    pdc_data_root: str = ''
+
+    # A list of file indicates which logs will be used for dataset.
+    scene_list_filepath: str = ''
+
+    # The name of object category
+    category_name_key: str = ''
+
+    # The name of the yaml file with pose annotation
+    # Relative to the "${pdc_data_root}/logs_proto/2018-10..../processed" folder
+    # Should be in ${pdc_data_root}/logs_proto/2018-10..../processed/${keypoint_yaml_name}
+    pose_yaml_name: str = 'images/pose_data.yaml'
+
+    # Simple flag
+    verbose: bool = True
+
+
+@attr.s
+class SpartanSingleObjMaskDatabaseEntry:
+    # The path to rgb
+    rgb_image_path = ''
+
+    # The path to depth image
+    depth_image_path = ''
+
+    # The path to mask image
+    binary_mask_path = ''
+
+    # The bounding box is tight
+    bbox_top_left = PixelCoord()
+    bbox_bottom_right = PixelCoord()
+
+
+class SpartanSingleObjMaskDatabase(AbstractMaskDatabase):
+
+    def __init__(self, config: SpartanSingleObjMaskDatabaseConfig):
+        super(SpartanSingleObjMaskDatabase, self).__init__()
+        self._config = config
+
+        # Build a list of scene that will be used as the dataset
+        self._scene_path_list = self._get_scene_from_config(config)
+
+        # For each scene
+        self._image_entry_list = []
+        for scene_root in self._scene_path_list:
+            # The info code
+            if config.verbose:
+                print('Processing: ', scene_root)
+
+            # The processing code
+            scene_entry = SpartanSingleObjMaskDatabase._build_scene_entry(scene_root, config.pose_yaml_name)
+            for item in scene_entry:
+                self._image_entry_list.append(item)
+
+        # Simple info
+        print('The number of images is %d' % len(self._image_entry_list))
+
+    def __len__(self):
+        return len(self._image_entry_list)
+
+    @property
+    def path_entry_list(self) -> List[SpartanSingleObjMaskDatabaseEntry]:
+        return self._image_entry_list
+
+    def __getitem__(self, idx: int) -> ImageWithAnnotation:
+        image_path_entry = self._image_entry_list[idx]
+        # The returned type
+        result = ImageWithAnnotation()
+
+        # The raw RGB image
+        result.rgb_image = cv2.imread(image_path_entry.rgb_image_path, cv2.IMREAD_ANYCOLOR)
+
+        # The annotation, there is only one object
+        annotation = AnnotationEntry()
+        annotation.category_name = self._config.category_name_key
+        annotation.binary_mask = cv2.imread(image_path_entry.binary_mask_path, cv2.IMREAD_GRAYSCALE)
+        annotation.bbox_topleft = image_path_entry.bbox_top_left
+        annotation.bbox_bottomright = image_path_entry.bbox_bottom_right
+
+        # Append to result and return
+        result.annotation_list.append(annotation)
+        return result
+
+    @staticmethod
+    def _get_scene_from_config(config: SpartanSingleObjMaskDatabaseConfig) -> List[str]:
+        assert os.path.exists(config.pdc_data_root)
+        assert os.path.exists(config.scene_list_filepath)
+
+        # Read the config file
+        scene_root_list = []
+        with open(config.scene_list_filepath, 'r') as config_file:
+            lines = config_file.read().split('\n')
+            for line in lines:
+                if len(line) == 0:
+                    continue
+                scene_root = os.path.join(config.pdc_data_root, line)
+                if SpartanSingleObjMaskDatabase._is_scene_valid(scene_root, config.pose_yaml_name):
+                    scene_root_list.append(scene_root)
+
+        # OK
+        return scene_root_list
+
+    @staticmethod
+    def _is_scene_valid(scene_root: str, pose_yaml_name: str) -> bool:
+        # The path must be valid
+        if not os.path.exists(scene_root):
+            return False
+
+        # Must contains keypoint annotation
+        scene_processed_root = os.path.join(scene_root, 'processed')
+        keypoint_yaml_path = os.path.join(scene_processed_root, pose_yaml_name)
+        if not os.path.exists(keypoint_yaml_path):
+            return False
+
+        # OK
+        return True
+
+    @staticmethod
+    def _build_scene_entry(scene_root: str, pose_yaml_name: str) -> List[SpartanSingleObjMaskDatabaseEntry]:
+        # Get the yaml file
+        scene_processed_root = os.path.join(scene_root, 'processed')
+        pose_yaml_path = os.path.join(scene_processed_root, pose_yaml_name)
+        assert os.path.exists(pose_yaml_path)
+
+        # Read the yaml map
+        pose_yaml_file = open(pose_yaml_path, 'r')
+        pose_yaml_map = yaml.load(pose_yaml_file, Loader=yaml.FullLoader)
+        pose_yaml_file.close()
+
+        # Iterate over image
+        entry_list = []
+        for image_key in pose_yaml_map.keys():
+            image_map = pose_yaml_map[image_key]
+            image_entry = SpartanSingleObjMaskDatabase._get_image_entry(image_map, scene_root)
+            if image_entry is not None:
+                entry_list.append(image_entry)
+
+        # Ok
+        return entry_list
+
+    @staticmethod
+    def _get_image_entry(image_map, scene_root: str) -> SpartanSingleObjMaskDatabaseEntry:
+        entry = SpartanSingleObjMaskDatabaseEntry()
+        # The path for rgb image
+        rgb_name = image_map['rgb_image_filename']
+        rgb_path = os.path.join(scene_root, 'processed/images/' + rgb_name)
+        assert os.path.exists(rgb_path)
+        entry.rgb_image_path = rgb_path
+
+        # The path for depth image
+        depth_name = image_map['depth_image_filename']
+        depth_path = os.path.join(scene_root, 'processed/images/' + depth_name)
+        assert os.path.exists(depth_path) # Spartan must have depth image
+        entry.depth_image_path = depth_path
+
+        # The path for mask image
+        mask_name = depth_name[0:6] + '_mask.png'
+        mask_path = os.path.join(scene_root, 'processed/image_masks/' + mask_name)
+        assert os.path.exists(mask_path)
+        entry.binary_mask_path = mask_path
+
+        # The image bounding box
+        top_left = PixelCoord()
+        bottom_right = PixelCoord()
+        if ('bbox_top_left_xy' in image_map) and ('bbox_bottom_right_xy' in image_map):
+            # Already in the image map
+            top_left.x, top_left.y = image_map['bbox_top_left_xy'][0], image_map['bbox_top_left_xy'][1]
+            bottom_right.x, bottom_right.y = image_map['bbox_bottom_right_xy'][0], image_map['bbox_bottom_right_xy'][1]
+        else:
+            # Compute from mask
+            mask_img = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+            top_left, bottom_right = mask2bbox(mask_img)
+        entry.bbox_top_left = top_left
+        entry.bbox_bottom_right = bottom_right
+
+        # OK
+        return entry
+
+
+# The debugging method
+def path_entry_sanity_check(entry):
+    if len(entry.rgb_image_path) < 1 or (not os.path.exists(entry.rgb_image_path)):
+        return False
+
+    if len(entry.rgb_image_path) >= 1 and (not os.path.exists(entry.depth_image_path)):
+        return False
+
+    if len(entry.rgb_image_path) >= 1 and (not os.path.exists(entry.binary_mask_path)):
+        return False
+
+    if (not entry.bbox_top_left.is_valid()) or (not entry.bbox_bottom_right.is_valid()):
+        return False
+
+    # OK
+    return True
+
+
+def test_spartan_singleobj_database():
+    config = SpartanSingleObjMaskDatabaseConfig()
+    config.pdc_data_root = '/home/wei/data/pdc'
+    config.scene_list_filepath = '/home/wei/Coding/fill_it/config/boot_logs.txt'
+    config.category_name_key = 'shoe'
+    database = SpartanSingleObjMaskDatabase(config)
+    path_entry_list = database.path_entry_list
+    for entry in path_entry_list:
+        assert path_entry_sanity_check(entry)
+
+
+if __name__ == '__main__':
+    test_spartan_singleobj_database()
--- utils/imgproc.py
+++ utils/imgproc.py
@@ -1,4 +1,6 @@
 import attr
+import numpy as np
+
 
 """ The default pixel coordinate type
 """
@@ -14,3 +16,67 @@ class PixelCoord:
     @property
     def col(self):
         return self.x
+
+    def is_valid(self) -> bool:
+        return self.x >= 0 and self.y >= 0
+
+
+# The group of method used for bounding box processing.
+# Mainly used to rectify the bounding box
+def first_nonzero_idx(binary_array: np.ndarray, reversed: bool) -> int:
+    """
+    Get the index of the first element in an array that is not zero
+    reversed means whether the binary_array should be reversed
+    :param binary_array: A 1-D numpy array
+    :param reversed:
+    :return: The index to the first non-zero element
+    """
+    start = 0
+    end = binary_array.size
+    step = 1
+    if reversed:
+        start = binary_array.size - 1
+        end = -1
+        step = -1
+
+    # The iteration
+    for i in range(start, end, step):
+        if binary_array[i] > 0:
+            return i
+
+    # Everything is zero
+    return None
+
+
+def mask2bbox(mask_img: np.ndarray) -> (PixelCoord, PixelCoord):
+    """
+    Given an object binary mask, get the tight object bounding box
+    as a tuple contains top_left and bottom_right pixel coord
+    :param mask_img: (height, width, 3) mask image
+    :return: A tuple contains top_left and bottom_right pixel coord
+    """
+    if len(mask_img.shape) == 2:
+        binary_mask = mask_img
+    else:
+        binary_mask = mask_img.max(axis=2)
+    n_rows, n_cols = binary_mask.shape
+    # Compute sum over the row and compute the left and right
+    mask_rowsum = np.sum(binary_mask, axis=0, keepdims=False)
+    assert mask_rowsum.size == n_cols
+    left = first_nonzero_idx(mask_rowsum, False)
+    right = first_nonzero_idx(mask_rowsum, True)
+
+    # Compute sum over the col and compute the top and bottom
+    mask_colsum = np.sum(binary_mask, axis=1)
+    assert mask_colsum.size == n_rows
+    top = first_nonzero_idx(mask_colsum, False)
+    bottom = first_nonzero_idx(mask_colsum, True)
+
+    # Ok
+    top_left = PixelCoord()
+    top_left.x = left
+    top_left.y = top
+    bottom_right = PixelCoord()
+    bottom_right.x = right
+    bottom_right.y = bottom
+    return top_left, bottom_right
